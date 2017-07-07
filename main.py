01a55a3a8cdb9ba33806bdb883be479dcd6328fa
Kristen Sharkey <kristen.l.sharkey@gmail.com>
2017-07-07 00:02:09 -0700

    added css and moved calc to calc template

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import math
+import cgi
 
 app= Flask(__name__)
 app.config['DEBUG']=True
@@ -11,24 +12,24 @@ def index():
 
 @app.route("/calculated", methods=['POST']) #handler
 def calculate():
-    denominator = 1
-    answer = ''
+    denominator = 1                                             #set denominator in case there are no frequencies chosen
+    answer = ''                                                 #var for final answer
     key_list=[]
     key_antigen = ''
     antigen_codes = {"0.32": "C", "0.20": "c", "0.71": "E", "0.02": "e", "0.34": "Fya", "0.17": "Fyb", "0.23": "Jka", "0.26": "Jkb", "0.78": "Lea", "0.28": "Leb", "0.22": "M",  "0.280": "N", "0.45": "S", "0.11": "s",  "0.91": "K"}
     list_values = list(antigen_codes.values())
     antigens = request.form.getlist('antigen')
-    units_transfuse = int(request.form['units_to_transfuse'])
+    units_transfuse = int(request.form[cgi.escape('units_to_transfuse')])   #make it an int rather than a str
+    for antigen in antigens:                                    #access each antigen in the list of checked antigens
+        key_antigen = antigen_codes.get(antigen)                #get the key (antigen name) of the frequency
+        key_list.append(key_antigen)                            #add key (antigen name) to list
     for antigen in antigens:
-        key_antigen = antigen_codes.get(antigen)
-        key_list.append(key_antigen)
-    for antigen in antigens:
-        antigen = float(antigen)
-        denominator *= antigen
-    calculation = (units_transfuse/denominator)
-    calculation = round(calculation, 2)
-    answer = math.ceil(calculation)
+        antigen = float(antigen)                                #make frequency a float not a str
+        denominator *= antigen                                  #multiply eacg frequency to get denom.
+    calculation = (units_transfuse/denominator)                 #calculation not rounded
+    calculation = round(calculation, 2)                         #round down to 2 decimal points
+    answer = math.ceil(calculation)                             #get closest int above calculation
 
-    return '<h1>Antigen Negative Units </h1><br><h2>Number of units total:<br><bold>'+str(answer)+'</bold></h2><br><h3>Units (to the hundreth place):</h3>'+str(calculation)+'<h3>You chose: <br>'+str(key_list).strip('[]')+'</h3>'
+    return render_template('calculation.html', answer=str(answer), calculation=str(calculation), key_list=str(key_list).strip('[]'))
     
 app.run()
